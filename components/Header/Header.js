89ae47cc55483e435909fc6423a045ce6ee26148
Stanislav <akhmadeevst@gmail.com>
2021-09-13 13:11:40 +0400

    add count products

---
+++ components/Header/Header.js
@@ -0,0 +1,14 @@
+class Header {
+  render(count) {
+    const html = `
+        <div class="header-container">
+            <div class="header-counter">🔥 ${count} </div>
+        </div>
+        `;
+    ROOT_HEADER.innerHTML = html;
+  }
+}
+
+const headerPage = new Header();
+const productsStore = localStorageUtils.getProducts();
+headerPage.render(productsStore.length);
